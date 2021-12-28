c9722281cb1f33c5209a7c17d830f4a786dd4bf9
dkotenko <you@example.com>
2021-12-28 13:06:01 +0300

    determ init

--- src/s21_determinant.c
+++ src/s21_determinant.c
@@ -1,9 +1,52 @@
 #include "s21_matrix.h"
 
+static double get_determinant(matrix_t *A)
+{
+	int side_size = A->rows
+	double minor[side_size][side_size];
+	int i, j, runloop, row, col;
+	int det = 0;
+	int sign = 1;
+																	//Initialize sign as 1 initially as first i=element needs to be positive while calculating determinant.
+	if(order == 1)
+   {
+   	return(*(matrix));													//return the determinant value for Single Element.
+   }
+   else
+	{
+		for(runloop = 0 ; runloop < order ; runloop++)				//need to run the loop order number of times.
+   	{
+      	row = 0, col = 0;													//Initialize row and column for Minor Matrix as (0,0).
+      	for(i = 0 ; i < order ; i++)									//Loop to go through rows.
+      	{
+   	   	for(j = 0 ; j < order ; j++)								//Loop to go through columns.
+         	{
+     	   		*((Minor+i*(order-1))+j)=0;							//Initialize corresponding Minor element as Zero.
+            	if(i != 0 && j != runloop)								//Consider only the elements which are not in the same row and column of considered minor.
+            	{
+            		*((Minor+row*(order-1))+col) = *((matrix+i*order)+j);			//Assign value to Minor from Main Matrix.
+            		col++;													//Go to next column.
+            		if(col > order-2)										//If column exceeds the Minor Matrix size.
+               	{
+               		row++;												//Go to next row.
+               		col=0;												//Go to first column of next row. 
+               	}
+					}
+				}
+			}
+         det = det + (sign * ((*(matrix+runloop)) * (determinant(Minor,order-1))));	//Recursive function to call the same function again with Minor size.
+         sign=-1*sign;
+		}
+	}
+   free(Minor);																//Free the allocated memory.
+   return (det);																//Return the value of determinant.
+}
+}
+
 double s21_determinant(matrix_t *A)
 {
 	if (A->rows != A->columns || A->rows < 1 || A->columns < 1) {
-		return 0;
+		return get_incorrect_matrix();
 	}
 	return 0;
 }
---
+++ tests/src/test_main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "color.h"
+
+void print_result(char *test_name, int result)
+{
+    printf("%s%s - %s%s\n",
+        result ? ANSI_COLOR_GREEN : ANSI_COLOR_RED,
+        test_name,
+        result ? "OK" : "FAILURE",
+        ANSI_COLOR_RESET);
+}
+
+int main()
+{
+    
+    return 0;
+}
